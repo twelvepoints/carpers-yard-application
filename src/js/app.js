9653d42f8cf9094fd4d818b4da2b5e59e25368a0
Dave Perry <dave@Daves-MacBook-Pro.local>
2019-12-08 23:02:16 +0000

    script changed to on load

--- src/js/app.js
+++ src/js/app.js
@@ -1,88 +1,88 @@
-const swiperFeatured = new Swiper('.featured-carousel .swiper-container', {
-    updateOnWindowResize: true,
-    observer: true,
-    roundLengths: true,
-    grabCursor: true,
-    pagination: {
-        el: '.swiper-pagination',
-        dynamicBullets: true,
-    },
-    initialSlide: 0,
-    spaceBetween: 8,
-    slidesPerView: 1.15,
-    breakpoints: {
-        768: {
-            slidesPerView: 2.5,
-            pagination: false,
-            clickable: true
-        },
-        1024: {
-            slidesPerView: 3,
-            pagination: false,
-            clickable: true
-        }
-    }
-});
-
-const swiperCard = new Swiper('.card-carousel .swiper-container', {
-    updateOnWindowResize: true,
-    observer: true,
-    roundLengths: true,
-    grabCursor: true,
-    freeModeMomentum: true,
-    initialSlide: 0,
-    spaceBetween: 8,
-    slidesPerView: 2.5,
-    breakpoints: {
-        768: {
-            slidesPerView: 3.5,
-            pagination: {
-                el: '.swiper-pagination',
-                dynamicBullets: true,
-            },
-            clickable: true
-        },
-        1024: {
-            slidesPerView: 5,
-            pagination: {
-                el: '.swiper-pagination',
-                dynamicBullets: true,
-            },
-            clickable: true
-        },
-        1366: {
-            slidesPerView: 6,
-            pagination: {
-                el: '.swiper-pagination',
-                dynamicBullets: true,
-            },
-            clickable: true
-        }
-    }
-});
-
-
-
 window.addEventListener("load", function() {
-	// store tabs variable
-	var myTabs = document.querySelectorAll("ul.nav-tabs > li");
-  function myTabClicks(tabClickEvent) {
-		for (var i = 0; i < myTabs.length; i++) {
-			myTabs[i].classList.remove("active");
-		}
-		var clickedTab = tabClickEvent.currentTarget;
-		clickedTab.classList.add("active");
-		tabClickEvent.preventDefault();
-		var myContentPanes = document.querySelectorAll(".tab-pane");
-		for (i = 0; i < myContentPanes.length; i++) {
-			myContentPanes[i].classList.remove("active");
-		}
-		var anchorReference = tabClickEvent.target;
-		var activePaneId = anchorReference.getAttribute("href");
-		var activePane = document.querySelector(activePaneId);
-		activePane.classList.add("active");
-	}
-	for (i = 0; i < myTabs.length; i++) {
-		myTabs[i].addEventListener("click", myTabClicks)
-	}
+    const swiperFeatured = new Swiper('.featured-carousel .swiper-container', {
+        updateOnWindowResize: true,
+        observer: true,
+        roundLengths: true,
+        grabCursor: true,
+        pagination: {
+            el: '.swiper-pagination',
+            dynamicBullets: true,
+        },
+        initialSlide: 0,
+        spaceBetween: 8,
+        slidesPerView: 1.15,
+        breakpoints: {
+            768: {
+                slidesPerView: 2.5,
+                pagination: false,
+                clickable: true
+            },
+            1024: {
+                slidesPerView: 3,
+                pagination: false,
+                clickable: true
+            }
+        }
+    });
+
+    const swiperCard = new Swiper('.card-carousel .swiper-container', {
+        updateOnWindowResize: true,
+        observer: true,
+        roundLengths: true,
+        grabCursor: true,
+        freeModeMomentum: true,
+        initialSlide: 0,
+        spaceBetween: 8,
+        slidesPerView: 2.5,
+        breakpoints: {
+            768: {
+                slidesPerView: 3.5,
+                pagination: {
+                    el: '.swiper-pagination',
+                    dynamicBullets: true,
+                },
+                clickable: true
+            },
+            1024: {
+                slidesPerView: 5,
+                pagination: {
+                    el: '.swiper-pagination',
+                    dynamicBullets: true,
+                },
+                clickable: true
+            },
+            1366: {
+                slidesPerView: 6,
+                pagination: {
+                    el: '.swiper-pagination',
+                    dynamicBullets: true,
+                },
+                clickable: true
+            }
+        }
+    });
+});
+
+window.addEventListener("load", function () {
+    // store tabs variable
+    var myTabs = document.querySelectorAll("ul.nav-tabs > li");
+    function myTabClicks(tabClickEvent) {
+        for (var i = 0; i < myTabs.length; i++) {
+            myTabs[i].classList.remove("active");
+        }
+        var clickedTab = tabClickEvent.currentTarget;
+        clickedTab.classList.add("active");
+        tabClickEvent.preventDefault();
+        var myContentPanes = document.querySelectorAll(".tab-pane");
+        for (i = 0; i < myContentPanes.length; i++) {
+            myContentPanes[i].classList.remove("active");
+        }
+        var anchorReference = tabClickEvent.target;
+        var activePaneId = anchorReference.getAttribute("href");
+        var activePane = document.querySelector(activePaneId);
+        activePane.classList.add("active");
+    }
+    for (i = 0; i < myTabs.length; i++) {
+        myTabs[i].addEventListener("click", myTabClicks)
+    }
 });
