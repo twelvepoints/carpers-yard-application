1e8b4a0bb171a2b09da16716361125c9cdb73487
Dave Perry <dave@Daves-MacBook-Pro.local>
2019-12-09 23:18:52 +0000

    changed sliders. added images

--- src/js/app.js
+++ src/js/app.js
@@ -1,67 +1,241 @@
-window.addEventListener("load", function() {
-    const swiperFeatured = new Swiper('.featured-carousel .swiper-container', {
-        updateOnWindowResize: true,
-        observer: true,
-        roundLengths: true,
-        grabCursor: true,
-        pagination: {
-            el: '.swiper-pagination',
-            dynamicBullets: true,
-        },
-        initialSlide: 0,
-        spaceBetween: 8,
-        slidesPerView: 1.15,
-        breakpoints: {
-            768: {
-                slidesPerView: 2.5,
-                pagination: false,
-                clickable: true
+window.addEventListener("load", function () {
+    let cardSlider = tns({
+        container: '.card-slider',
+        loop: false,
+        mouseDrag: true,
+        swipeAngle: true,
+        autoplay: false,
+        controls: false,
+        nav: false,
+        startIndex: 0,
+        responsive: {
+            0: {
+                edgePadding: 24,
+                gutter: 8,
+                items: 2
+            },
+            786: {
+                edgePadding: 24,
+                gutter: 8,
+                items: 4
             },
             1024: {
-                slidesPerView: 3,
-                pagination: false,
-                clickable: true
+                edgePadding: 32,
+                gutter: 16,
+                items: 5
             }
         }
     });
 
-    const swiperCard = new Swiper('.card-carousel .swiper-container', {
-        updateOnWindowResize: true,
-        observer: true,
-        roundLengths: true,
-        grabCursor: true,
-        freeModeMomentum: true,
-        initialSlide: 0,
-        spaceBetween: 8,
-        slidesPerView: 2.5,
-        breakpoints: {
-            768: {
-                slidesPerView: 3.5,
-                pagination: {
-                    el: '.swiper-pagination',
-                    dynamicBullets: true,
-                },
-                clickable: true
+    let cardSlider2 = tns({
+        container: '.card-slider-2',
+        loop: false,
+        mouseDrag: true,
+        swipeAngle: true,
+        autoplay: false,
+        controls: false,
+        nav: false,
+        startIndex: 0,
+        responsive: {
+            0: {
+                edgePadding: 24,
+                gutter: 8,
+                items: 2
+            },
+            786: {
+                edgePadding: 24,
+                gutter: 8,
+                items: 4
             },
             1024: {
-                slidesPerView: 5,
-                pagination: {
-                    el: '.swiper-pagination',
-                    dynamicBullets: true,
-                },
-                clickable: true
+                edgePadding: 32,
+                gutter: 16,
+                items: 5
+            }
+        }
+    });
+
+    let cardSlider3 = tns({
+        container: '.card-slider-3',
+        loop: false,
+        mouseDrag: true,
+        swipeAngle: true,
+        autoplay: false,
+        controls: false,
+        nav: false,
+        startIndex: 0,
+        responsive: {
+            0: {
+                edgePadding: 24,
+                gutter: 8,
+                items: 2
             },
-            1366: {
-                slidesPerView: 6,
-                pagination: {
-                    el: '.swiper-pagination',
-                    dynamicBullets: true,
-                },
-                clickable: true
+            786: {
+                edgePadding: 24,
+                gutter: 8,
+                items: 4
+            },
+            1024: {
+                edgePadding: 32,
+                gutter: 16,
+                items: 5
+            }
+        }
+    });
+
+    let cardSlider4 = tns({
+        container: '.card-slider-4',
+        loop: false,
+        mouseDrag: true,
+        swipeAngle: true,
+        autoplay: false,
+        controls: false,
+        nav: false,
+        startIndex: 0,
+        responsive: {
+            0: {
+                edgePadding: 24,
+                gutter: 8,
+                items: 2
+            },
+            786: {
+                edgePadding: 24,
+                gutter: 8,
+                items: 4
+            },
+            1024: {
+                edgePadding: 32,
+                gutter: 16,
+                items: 5
+            }
+        }
+    });
+
+    let cardSlider5 = tns({
+        container: '.card-slider-5',
+        loop: false,
+        mouseDrag: true,
+        swipeAngle: true,
+        autoplay: false,
+        controls: false,
+        nav: false,
+        startIndex: 0,
+        responsive: {
+            0: {
+                edgePadding: 24,
+                gutter: 8,
+                items: 2
+            },
+            786: {
+                edgePadding: 24,
+                gutter: 8,
+                items: 4
+            },
+            1024: {
+                edgePadding: 32,
+                gutter: 16,
+                items: 5
             }
         }
     });
 });
+
+window.addEventListener("load", function () {
+    let featuredSlider = tns({
+        container: '.featured-slider',
+        loop: false,
+        mouseDrag: true,
+        swipeAngle: true,
+        autoplay: false,
+        controls: false,
+        nav: false,
+        startIndex: 0,
+        responsive: {
+            0: {
+                edgePadding: 24,
+                gutter: 8,
+                items: 1
+            },
+            786: {
+                edgePadding: 24,
+                gutter: 8,
+                items: 2.5
+            },
+            1024: {
+                edgePadding: 32,
+                gutter: 16,
+                items: 2.99
+            }
+        }
+    });
+});
+
+
+
+// window.addEventListener("load", function () {
+//     const swiperFeatured = new Swiper('.featured-carousel .swiper-container', {
+//         updateOnWindowResize: true,
+//         observer: true,
+//         roundLengths: true,
+//         grabCursor: true,
+//         pagination: {
+//             el: '.swiper-pagination',
+//             dynamicBullets: true,
+//         },
+//         initialSlide: 0,
+//         spaceBetween: 8,
+//         slidesPerView: 1.15,
+//         breakpoints: {
+//             768: {
+//                 slidesPerView: 2.5,
+//                 pagination: false,
+//                 clickable: true
+//             },
+//             1024: {
+//                 slidesPerView: 3,
+//                 pagination: false,
+//                 clickable: true
+//             }
+//         }
+//     });
+
+//     const swiperCard = new Swiper('.card-carousel .swiper-container', {
+//         updateOnWindowResize: true,
+//         observer: true,
+//         roundLengths: true,
+//         grabCursor: true,
+//         freeModeMomentum: true,
+//         initialSlide: 0,
+//         spaceBetween: 8,
+//         slidesPerView: 2.5,
+//         breakpoints: {
+//             768: {
+//                 slidesPerView: 3.5,
+//                 pagination: {
+//                     el: '.swiper-pagination',
+//                     dynamicBullets: true,
+//                 },
+//                 clickable: true
+//             },
+//             1024: {
+//                 slidesPerView: 5,
+//                 pagination: {
+//                     el: '.swiper-pagination',
+//                     dynamicBullets: true,
+//                 },
+//                 clickable: true
+//             },
+//             1366: {
+//                 slidesPerView: 6,
+//                 pagination: {
+//                     el: '.swiper-pagination',
+//                     dynamicBullets: true,
+//                 },
+//                 clickable: true
+//             }
+//         }
+//     });
+// });
 
 window.addEventListener("load", function () {
     // store tabs variable
